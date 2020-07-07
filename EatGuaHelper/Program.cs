76a3da906a22f02042fad7fe9e358292011227d9
尼古拉斯赵四 <185653517@qq.com>
2020-07-07 21:21:51 +0800

    Add        .vs/EatGuaHelper/v16/.suo Add        EatGuaHelper.sln Add        EatGuaHelper/App.config Add        EatGuaHelper/EatGuaHelper.csproj Add        EatGuaHelper/Program.cs Add        EatGuaHelper/Properties/AssemblyInfo.cs Add        EatGuaHelper/bin/Debug/EatGuaHelper.exe Add        EatGuaHelper/bin/Debug/EatGuaHelper.exe.config Add        EatGuaHelper/bin/Debug/EatGuaHelper.pdb Add        EatGuaHelper/bin/Debug/Newtonsoft.Json.dll Add        EatGuaHelper/bin/Debug/Newtonsoft.Json.xml Add        EatGuaHelper/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache Add        EatGuaHelper/obj/Debug/EatGuaHelper.csproj.CopyComplete Add        EatGuaHelper/obj/Debug/EatGuaHelper.csproj.CoreCompileInputs.cache Add        EatGuaHelper/obj/Debug/EatGuaHelper.csproj.FileListAbsolute.txt Add        EatGuaHelper/obj/Debug/EatGuaHelper.csprojAssemblyReference.cache Add        EatGuaHelper/obj/Debug/EatGuaHelper.exe Add        EatGuaHelper/obj/Debug/EatGuaHelper.pdb Add        EatGuaHelper/packages.config Add        packages/Newtonsoft.Json.12.0.3/.signature.p7s Add        packages/Newtonsoft.Json.12.0.3/LICENSE.md Add        packages/Newtonsoft.Json.12.0.3/Newtonsoft.Json.12.0.3.nupkg Add        packages/Newtonsoft.Json.12.0.3/lib/net20/Newtonsoft.Json.dll Add        packages/Newtonsoft.Json.12.0.3/lib/net20/Newtonsoft.Json.xml Add        packages/Newtonsoft.Json.12.0.3/lib/net35/Newtonsoft.Json.dll Add        packages/Newtonsoft.Json.12.0.3/lib/net35/Newtonsoft.Json.xml Add        packages/Newtonsoft.Json.12.0.3/lib/net40/Newtonsoft.Json.dll Add        packages/Newtonsoft.Json.12.0.3/lib/net40/Newtonsoft.Json.xml Add        packages/Newtonsoft.Json.12.0.3/lib/net45/Newtonsoft.Json.dll Add        packages/Newtonsoft.Json.12.0.3/lib/net45/Newtonsoft.Json.xml Add        packages/Newtonsoft.Json.12.0.3/lib/netstandard1.0/Newtonsoft.Json.dll Add        packages/Newtonsoft.Json.12.0.3/lib/netstandard1.0/Newtonsoft.Json.xml Add        packages/Newtonsoft.Json.12.0.3/lib/netstandard1.3/Newtonsoft.Json.dll Add        packages/Newtonsoft.Json.12.0.3/lib/netstandard1.3/Newtonsoft.Json.xml Add        packages/Newtonsoft.Json.12.0.3/lib/netstandard2.0/Newtonsoft.Json.dll Add        packages/Newtonsoft.Json.12.0.3/lib/netstandard2.0/Newtonsoft.Json.xml Add        packages/Newtonsoft.Json.12.0.3/lib/portable-net40+sl5+win8+wp8+wpa81/Newtonsoft.Json.dll Add        packages/Newtonsoft.Json.12.0.3/lib/portable-net40+sl5+win8+wp8+wpa81/Newtonsoft.Json.xml Add        packages/Newtonsoft.Json.12.0.3/lib/portable-net45+win8+wp8+wpa81/Newtonsoft.Json.dll Add        packages/Newtonsoft.Json.12.0.3/lib/portable-net45+win8+wp8+wpa81/Newtonsoft.Json.xml Add        packages/Newtonsoft.Json.12.0.3/packageIcon.png

---
+++ EatGuaHelper/Program.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EatGuaHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime? TimerFlag = null;
+            while (true)
+            {
+                if (TimerFlag == null || TimerFlag < DateTime.Now)
+                {
+                    using (WebClient webClient = new WebClient())
+                    {
+                        #region 新浪
+                        //新浪热搜接口
+                        string SinaUrl = "https://s.weibo.com/ajax/jsonp/gettopsug";
+                        //调用接口
+                        string SinaResponseData = Encoding.UTF8.GetString(webClient.DownloadData(SinaUrl));
+                        //处理结果
+                        var SinaResult = JsonConvert.DeserializeObject<dynamic>(SinaResponseData.Replace("try{window.&({\"data\":", "").Replace("})}catch(e){}", ""));
+                        using (SpeechSynthesizer reader = new SpeechSynthesizer())
+                        {
+                            string SinaText = string.Empty;
+                            for (int i = 0; i < SinaResult.list.Count; i++)
+                            {
+                                SinaText = "新浪热搜第" + (i + 1) + "名:" + SinaResult.list[i].note.ToString() + ";";
+                                Console.WriteLine(DateTime.Now + SinaText);
+                                reader.Speak(SinaText);
+                            }
+                            reader.Dispose();
+                        }
+                        #endregion
+                        #region 知乎热榜
+                        //知乎热榜接口
+                        string ZhihuUrl = "https://www.zhihu.com/api/v3/feed/topstory/hot-lists/total?limit=50&desktop=true";
+                        //调用接口
+                        string ZhihuResponseData = Encoding.UTF8.GetString(webClient.DownloadData(ZhihuUrl));
+                        //处理结果
+                        var ZhihuResult = JsonConvert.DeserializeObject<dynamic>(ZhihuResponseData);
+                        using (SpeechSynthesizer reader = new SpeechSynthesizer())
+                        {
+                            string ZhihuText = string.Empty;
+                            for (int i = 0; i < 10; i++)
+                            {
+                                ZhihuText = "知乎热榜第" + (i + 1) + "名:" + ZhihuResult.data[i].target.title.ToString() + ";";
+                                Console.WriteLine(DateTime.Now + ZhihuText);
+                                reader.Speak(ZhihuText);
+                            }
+                            reader.Dispose();
+                        }
+                        #endregion
+                    }
+                    Console.WriteLine("----------------------------------截止时间:" + DateTime.Now + "----------------------------------");
+                    TimerFlag = DateTime.Now.AddMinutes(30);
+                }
+
+            }
+        }
+    }
+}
